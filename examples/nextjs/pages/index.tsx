22a7bf79240fdc2798c9c599c15b2843f3cc4f5a
Danilo Woznica <danilowoz@gmail.com>
2022-08-17 10:21:26 +0100

    vite test

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,10 +1,25 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
 import { githubLight, sandpackDark } from "@codesandbox/sandpack-themes";
 
+import lodash from "raw-loader!lodash";
+
 const Home: React.FC = () => {
   return (
     <div>
-      <Sandpack />
+      <Sandpack
+        template="vanilla"
+        files={{
+          "/src/index.js": `import { chunk } from "lodash";
+          
+console.log(chunk(['a', 'b', 'c', 'd'], 2))
+          `,
+          "/node_modules/lodash/package.json": JSON.stringify({
+            name: "lodash",
+            main: "./index.js",
+          }),
+          "/node_modules/lodash/index.js": lodash,
+        }}
+      />
       <Sandpack theme="dark" />
       <Sandpack theme={githubLight} />
       <Sandpack options={{ readOnly: true }} theme={sandpackDark} />
