a128d824cc0bdfdd555aadad531c3c59c1bc52d1
Danilo Woznica <danilowoz@gmail.com>
2022-10-07 11:54:10 +0000

    Update index.tsx

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,13 +1,21 @@
+import { SandpackLogLevel } from "@codesandbox/sandpack-client";
 import { Sandpack } from "@codesandbox/sandpack-react";
 import { githubLight, sandpackDark } from "@codesandbox/sandpack-themes";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Sandpack />
-      <Sandpack theme="dark" />
+      <Sandpack
+        template="react"
+        options={{
+          bundlerURL: "https://lkdt8x-1234.preview.csb.app/",
+          logLevel: SandpackLogLevel.Debug,
+        }}
+      />
+
+      {/* <Sandpack theme="dark" />
       <Sandpack theme={githubLight} />
-      <Sandpack options={{ readOnly: true }} theme={sandpackDark} />
+      <Sandpack options={{ readOnly: true }} theme={sandpackDark} /> */}
     </div>
   );
 };
